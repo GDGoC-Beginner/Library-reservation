356df8ffb3053923647358e8ec360f460fbfcae4
박선준 <tjswns0437@naver.com>
2026-01-29 23:42:28 +0900

    Add files via upload

---
+++ backend/LMS/src/main/java/com/example/LMS/domain/user/dto/SignUpRequest.java
@@ -0,0 +1,13 @@
+package com.example.LMS.domain.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequest {
+    private String username; // 필수, UNIQUE
+    private String email;    // 필수
+    private String password; // 필수, 단방향 해시 적용 대상
+    private String name;     // 필수
+}
